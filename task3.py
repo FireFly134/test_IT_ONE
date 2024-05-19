5e2b48e64e879a07c720f9a0e1109dda87c1c43e
Константин <menace34@bk.ru>
2024-05-19 18:44:51 +0300

    correction files

--- task3.py
+++ task3.py
@@ -1,6 +1,3 @@
-# a = [[1,2,3], [4,5,6]]
-# сделать список словарей
-# b = [{'k1': 1, 'k2': 2, 'k3': 3}, {'k1': 4, 'k2': 5, 'k3': 6}]
 def main(a: list[list[int]]) -> list[dict[str, int]]:
     b = list()
     for num_list in a:
--- task4.py
+++ task4.py
@@ -1,22 +1,15 @@
-import os
 import logging
+import os
 from datetime import datetime, timedelta
 
 
 def delete_old_files(folder_path: str, days: int) -> None:
-
     now = datetime.now()
     cutoff = now - timedelta(days=days)
-
-    # Перебираем все файлы в папке
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
-
-        # Проверяем, является ли это файлом
         if os.path.isfile(file_path):
-            # Получаем время последнего изменения файла
             file_modified_time = os.path.getmtime(file_path)
-            # Если файл старше N дней, удаляем его
             if file_modified_time < cutoff.timestamp():
                 logging.info(f"Удаляю файл: {file_path}")
                 os.remove(file_path)
