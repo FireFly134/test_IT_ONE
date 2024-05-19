9119e3cab8c90d766d5c18d943e82f18dd919f46
Константин <menace34@bk.ru>
2024-05-19 21:40:03 +0300

    Upgrade cod, added solution one line (*)

--- task3.py
+++ task3.py
@@ -1,13 +1,19 @@
 def main(a: list[list[int]]) -> list[dict[str, int]]:
-    b = list()
+    b: list = list()
     for num_list in a:
-        dict_num = dict()
+        dict_num: dict = dict()
         for i, num in enumerate(num_list):
-            dict_num[f"k{i}"] = num
+            dict_num[f"k{i}"]: int = num
         else:
             b.append(dict_num)
     return b
 
 
+def main_one_line(a: list[list[int]]) -> list[dict[str, int]]:
+    return [{f"k{i}": num for i, num in enumerate(num_list)} for num_list in a]
+
+
 if __name__ == "__main__":
-    print(main(a=[[1, 2, 3], [4, 5, 6]]))
+    a = [[1, 2, 3], [4, 5, 6]]
+    print(main(a))
+    print(main_one_line(a))
